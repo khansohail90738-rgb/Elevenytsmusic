087d85ad084e0e303f5ab10943648af0aefc0efd
khansohail90738-rgb <khansohail90738@gmail.com>
2026-05-02 13:37:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ class Config:
         # ============ SUPPORT LINKS ============
         self.SUPPORT_CHANNEL: str = getenv(
             "SUPPORT_CHANNEL", "https://t.me/Leob2343")
-        self.SUPPORT_CHAT: str = getenv("SUPPORT_CHAT", "https://t.me/LEOXst3")
+        self.SUPPORT_CHAT: str = getenv("SUPPORT_CHAT", "https://t.me/Leob2343")
         # ============ EXCLUDED CHATS ============
         # Parse comma-separated chat IDs that assistants should never leave
         self.EXCLUDED_CHATS: List[int] = self._parse_excluded_chats()
